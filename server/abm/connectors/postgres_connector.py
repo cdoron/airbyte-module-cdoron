700e6b3ba8949d76dfd166b3bd2cf7343b1486cc
Doron Chen <cdoron@il.ibm.com>
2022-01-23 20:44:55 +0200

    reading of dataset
    Signed-off-by: Doron Chen <cdoron@il.ibm.com>

--- server/abm/connectors/postgres_connector.py
+++ server/abm/connectors/postgres_connector.py
@@ -36,3 +36,24 @@ class PostgresConnector:
             tmp_config.flush()
             airbyte_catalog = self.run_container('discover --config ' +
                 tmp_config.name)[0]
+            catalog_json = json.loads(airbyte_catalog)
+
+            streams = []
+            for stream in catalog_json['catalog']['streams']:
+                stream_dict = {}
+                stream_dict['sync_mode'] = 'full_refresh'
+                stream_dict['stream'] = {}
+                stream_dict['stream']['source_defined_cursor'] = False
+                stream_dict['stream']['name'] = stream['name']
+                stream_dict['stream']['namespace'] = stream['namespace']
+                stream_dict['stream']['supported_sync_modes'] = \
+                    stream['supported_sync_modes']
+                stream_dict['stream']['json_schema'] = stream['json_schema']
+                streams.append(stream_dict)
+
+            with tempfile.NamedTemporaryFile(dir='/json') as tmp_configured_catalog:
+                tmp_configured_catalog.write(json.dumps({'streams': streams}).encode('utf-8'))
+                tmp_configured_catalog.flush()
+                dataset=self.run_container('read --config ' + tmp_config.name +
+                            ' --catalog ' + tmp_configured_catalog.name)
+        print(dataset)
